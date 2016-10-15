c46968ab13403e6e62a5d16b0dc486d89d4c8fc0
Thea <theakoutsoukis@gmail.com>
2016-10-15 15:19:16 +1000

    Changed display format for Patient details

--- HungryCells/Data/HeartRecordInitializer.cs
+++ HungryCells/Data/HeartRecordInitializer.cs
@@ -10,14 +10,14 @@ namespace HungryCells.Data
         {
             var patients = new List<Patient>
             {
-                new Patient{PatientID = 123456, FirstName="Carson",LastName="Alexander",BirthDate=DateTime.Parse("1955-12-01"),ReferralDate=DateTime.Parse("2016-10-13")},
-                new Patient{PatientID = 123457, FirstName="Meredith",LastName="Alonso",BirthDate=DateTime.Parse("1982-09-19"),ReferralDate=DateTime.Parse("2016-03-12")},
-                new Patient{PatientID = 123458, FirstName="Arturo",LastName="Anand",BirthDate=DateTime.Parse("1947-10-15"),ReferralDate=DateTime.Parse("2016-08-16")},
-                new Patient{PatientID = 123459, FirstName="Gytis",LastName="Barzdukas",BirthDate=DateTime.Parse("1945-02-22"),ReferralDate=DateTime.Parse("2016-02-02")},
-                new Patient{PatientID = 123450, FirstName="Yan",LastName="Li",BirthDate=DateTime.Parse("1929-04-04"),ReferralDate=DateTime.Parse("2016-10-13")},
-                new Patient{PatientID = 123451, FirstName="Peggy",LastName="Justice",BirthDate=DateTime.Parse("1963-05-10"),ReferralDate=DateTime.Parse("2016-07-04")},
-                new Patient{PatientID = 123452, FirstName="Laura",LastName="Norman",BirthDate=DateTime.Parse("1942-07-19"),ReferralDate=DateTime.Parse("2016-06-10")},
-                new Patient{PatientID = 123453, FirstName="Nino",LastName="Olivetto",BirthDate=DateTime.Parse("1956-03-30"),ReferralDate=DateTime.Parse("2016-02-20")}
+                new Patient{PatientID = 123456, FirstName="Carson",LastName="Alexander",BirthDate=DateTime.Parse("1955-12-01"),ReferralDate=DateTime.Parse("2016-10-13"), DVA=true},
+                new Patient{PatientID = 123457, FirstName="Meredith",LastName="Alonso",BirthDate=DateTime.Parse("1982-09-19"),ReferralDate=DateTime.Parse("2016-03-12"), DVA=true},
+                new Patient{PatientID = 123458, FirstName="Arturo",LastName="Anand",BirthDate=DateTime.Parse("1947-10-15"),ReferralDate=DateTime.Parse("2016-08-16"), DVA=false},
+                new Patient{PatientID = 123459, FirstName="Gytis",LastName="Barzdukas",BirthDate=DateTime.Parse("1945-02-22"),ReferralDate=DateTime.Parse("2016-02-02"), DVA=false},
+                new Patient{PatientID = 123450, FirstName="Yan",LastName="Li",BirthDate=DateTime.Parse("1929-04-04"),ReferralDate=DateTime.Parse("2016-10-13"), DVA=false},
+                new Patient{PatientID = 123451, FirstName="Peggy",LastName="Justice",BirthDate=DateTime.Parse("1963-05-10"),ReferralDate=DateTime.Parse("2016-07-04"), DVA=true},
+                new Patient{PatientID = 123452, FirstName="Laura",LastName="Norman",BirthDate=DateTime.Parse("1942-07-19"),ReferralDate=DateTime.Parse("2016-06-10"), DVA=false},
+                new Patient{PatientID = 123453, FirstName="Nino",LastName="Olivetto",BirthDate=DateTime.Parse("1956-03-30"),ReferralDate=DateTime.Parse("2016-02-20"), DVA=false}
             };
 
             patients.ForEach(s => context.Patients.Add(s));
--- HungryCells/Models/Patient.cs
+++ HungryCells/Models/Patient.cs
@@ -1,22 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace HungryCells.Models
 {
     public class Patient
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [DisplayName("UR")]
         public int PatientID { get; set; }
+        
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+
+        [DisplayName("DOB")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime BirthDate { get; set; }
-        public DateTime ReferralDate { get; set; }
+
+        [DisplayName("Referral Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? ReferralDate { get; set; }
+
+        [DisplayName("Referral Source")]
         public string ReferralSource { get; set; }
+
+        [DisplayName("Wait Listed")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? WaitListed { get; set; }
+
         public bool? DVA { get; set; }
         public string Status { get; set; }
         public string Procedure { get; set; }
+
+        [DisplayName("Valve Type")]
+        public string ValveType { get; set; }
 
         public virtual ICollection<Test> Tests { get; set; }
 
