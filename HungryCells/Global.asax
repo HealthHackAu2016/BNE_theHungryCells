57b403db6c1f69becd70ca5e9c0f34df49a714d5
LOUIS <LOUIS@LOUIS-PC>
2016-10-15 10:02:08 +1000

    Add project files.

---
+++ HungryCells/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HungryCells.MvcApplication" Language="C#" %>
---
+++ HungryCells/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HungryCells.Startup))]
+namespace HungryCells
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
