e42d6ad1b415fedaaf9890bdcb9d3a0123fd55a7
Thea <theakoutsoukis@gmail.com>
2016-10-15 14:07:29 +1000

    Defined Patient and Test tables, started working on views

---
+++ HungryCells/Data/HeartRecordContext.cs
@@ -0,0 +1,17 @@
+﻿using HungryCells.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace HungryCells.Data
+{
+    public class HeartRecordContext : DbContext
+    {
+        public HeartRecordContext() : base("HeartRecordContext")
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Test> Tests { get; set; }
+
+    }
+}
